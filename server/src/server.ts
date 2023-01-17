7de544370119b8229bbce8baa46af98df5c51bcb
cjatoba <claytonjatoba@gmail.com>
2023-01-17 09:10:02 -0300

    Configure Prisma and Cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,22 @@
-console.log('here')
+import FastiFy from 'fastify'
+import cors from '@fastify/cors'
+import {PrismaClient} from '@prisma/client'
+
+const app = FastiFy()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
+})
+
+const serverPort = 3333
+
+app.listen({
+  port: serverPort,
+}).then(() => {
+  console.log(`Server running in port ${serverPort}!`)
+})
